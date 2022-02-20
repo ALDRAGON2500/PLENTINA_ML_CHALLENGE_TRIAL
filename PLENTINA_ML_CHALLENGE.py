fbc1f6d5cf881bb6ccc4f5a51d28acd8c6133d91
ALDRAGON2500 <99462687+ALDRAGON2500@users.noreply.github.com>
2022-02-20 17:45:52 +0800

    Update PLENTINA_ML_CHALLENGE.py

--- PLENTINA_ML_CHALLENGE.py
+++ PLENTINA_ML_CHALLENGE.py
@@ -125,5 +125,5 @@ def fraud_detection():
 
 
 if __name__=='__main__':
-    #app.run(debug=True,use_reloader=False)
-    app.run(debug=True)
+    app.run(debug=True,use_reloader=False)
+    #app.run(debug=True)
